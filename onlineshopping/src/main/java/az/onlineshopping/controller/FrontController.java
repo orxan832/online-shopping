b865dead134749ae098fd60bf2d4b51c3eb2696f
Orxan Abbasli <orxan832@gmail.com>
2018-10-27 15:56:49 +0400

    Changes save successfully!

---
+++ onlineshopping/src/main/java/az/onlineshopping/controller/FrontController.java
@@ -0,0 +1,41 @@
+package az.onlineshopping.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class FrontController {
+	
+	@RequestMapping(value= { "/", "/home", "/index"})
+	public ModelAndView index() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", "Welcome My Spring Mvc Project");
+		return mv;
+		
+		
+	}
+	
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting ==null) {
+//			greeting="Orxan";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
+	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable(value="greeting", required=false)String greeting) {
+		if(greeting ==null) {
+			greeting="Orxan";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
+
+}
